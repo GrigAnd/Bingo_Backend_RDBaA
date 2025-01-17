efe6ccd6bf01f685de3b410a143c2bb93d61e772
netwid <me@netwid.dev>
2025-01-17 23:36:00 +0300

    getUser improve

--- module/getUser.js
+++ module/getUser.js
@@ -1,23 +1,29 @@
 const fetch = require('node-fetch');
 
-let cache = {};
-let user = {
-  name: "test test",
-  ava: "response?.response[0]?.photo_50"
-};
+const cache = {};
 
 module.exports = async (_id) => {
-  if (_id in cache) {
+  if (cache[_id]) {
     return cache[_id]
-  } else {
-   let resp = await fetch(`https://api.vk.com/method/users.get?access_token=${process.env.TOKEN}&v=5.140&fields=photo_100&user_id=${_id}`)
-   let response = await resp.json()
-        user = {
-          name: response?.response[0]?.first_name + " " + response?.response[0]?.last_name,
-          ava: response?.response[0]?.photo_100
-        }
-     cache[_id] = user
-        return user
-    
+  }
+
+  try {
+    const resp = await fetch(`https://api.vk.com/method/users.get?access_token=${process.env.TOKEN}&v=5.140&fields=photo_100&user_id=${_id}`)
+    const data = await resp.json()
+    const userData = data?.response?.[0]
+
+    if (userData) {
+      const user = {
+        name: `${userData.first_name} ${userData.last_name}`,
+        ava: userData.photo_100
+      }
+      cache[_id] = user
+      return user
+    } else {
+      throw new Error('User data not found')
+    }
+  } catch (error) {
+    console.error('Error fetching user data:', error)
+    return null
   }
 }
